e63ee5e6f5607fd48e9c47a641862c1c16c2bddc
sharonraju143 <sharon.60494@gmail.com>
2022-09-30 17:22:32 +0530

    changes

---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,14 @@
+namespace TestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int result = 1;
+            //Assert
+            Assert.AreEqual(result, 1);
+        }
+    }
+}
